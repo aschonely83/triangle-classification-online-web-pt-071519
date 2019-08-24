0764c80a5f915cabd960d1f7c0f3456255d0fc56
Adam Schonely <aschonely@gmail.com>
2019-08-24 00:55:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,10 +8,6 @@ class Triangle
   end 
   
   def kind
-    if (a <= 0) || (b <= 0) || (c <= 0)
-      raise TriangleError
-    elsif (a + b  <= c) || (a + c <= b) || (b + c <= a)
-      raise TriangleError
     validate_triangle
     if a == b && b == c
       :equilateral
