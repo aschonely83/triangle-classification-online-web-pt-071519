de77f5da91d9473895c7d3e6b3156a0965558379
Adam Schonely <aschonely@gmail.com>
2019-08-24 00:40:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ class Triangle
   end
   
   def validate_triangle
-    real_triangle = [(a + b > c), (a + c > b), ]
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
   
   class TriangleError
   end  
